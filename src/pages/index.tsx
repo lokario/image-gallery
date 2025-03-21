81bd7a8757c5f46098460d32c549dad6ebf5a4af
Juan Diaz <32882631+lokario@users.noreply.github.com>
2025-03-21 18:07:14 -0400

    feat: display ImageGrid in home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,27 @@
+import { Container, Typography, Box } from "@mui/material";
+import ImageGrid from "../components/ImageGrid";
+
 export default function Home() {
-	return <></>;
+	return (
+		<Container
+			maxWidth="lg"
+			sx={{ py: 4 }}>
+			<Box
+				textAlign="center"
+				mb={4}>
+				<Typography
+					variant="h4"
+					component="h1"
+					gutterBottom>
+					Mars Rover Image Gallery
+				</Typography>
+				<Typography
+					variant="subtitle1"
+					color="text.secondary">
+					Explore photos taken by NASA’s Curiosity rover on Mars
+				</Typography>
+			</Box>
+			<ImageGrid />
+		</Container>
+	);
 }
